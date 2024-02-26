4882bc09c39b7da50b824acd5131afbddaed9839
RobertoSamael <skateangel99@gmail.com>
2024-02-26 01:26:12 -0600

    Board added

---
+++ src/components/Board.js
@@ -0,0 +1,16 @@
+"use client"
+import React from 'react'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+
+export default function Board() {
+
+
+    
+  return (
+    <div className='h-full flex bg-indigo-200 p-5'>
+        <DragDropContext>
+
+        </DragDropContext>
+    </div>
+  )
+}
